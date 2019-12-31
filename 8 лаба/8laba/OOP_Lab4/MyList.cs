b49b48cc4fef81819fa2b6b529ca112db73cbdb7
BekhAndrey <54837935+BekhAndrey@users.noreply.github.com>
2019-12-31 11:54:10 +0300

    Add files via upload

---
+++ 8 лаба/8laba/OOP_Lab4/MyList.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace OOP_Lab4
+{
+    public class MyList<T> : IInterface<T>/* where T: Person*/
+    {
+        // ПОЛЯ 
+        private List<T> collection;
+      
+
+        // СВОЙСТВА
+        public int Size { get; }
+
+        // КОНСТРУКТОРЫ 
+        public MyList()
+        {
+            this.collection = new List<T>();
+        }
+
+
+
+        public void Add(T item)
+        {
+            collection.Add(item);
+        }
+        public void Del(T item)
+        {
+            collection.Remove(item);
+        }
+        public void Show()
+        {
+            foreach (T item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public int GetSize()
+        {
+            int size = 0;
+            foreach(T item in collection)
+            {
+                size++;
+            }
+            return size;
+        }
+        // ПЕРЕГРУЗКИ =================================================
+        public static MyList<T> operator + (MyList<T> list, T item)
+        {
+            list.collection.Add(item);
+            return list;
+        }
+
+        public static MyList<T> operator + (MyList<T> listOne, MyList<T> listTwo)
+        {
+            foreach(T item in listTwo.collection)
+            {
+                listOne.Add(item);
+            }
+
+            return listOne;
+        }
+
+        public static MyList<T> operator -- (MyList<T> list)
+        {
+            list.collection.RemoveAt(0);
+            return list;
+        }
+
+
+        public static bool operator true (MyList<T> list)
+        {
+            if (list.GetSize() == 0)
+                return true;
+            else return false;
+        }
+
+        public static bool operator false (MyList<T> list)
+        {
+            if (list.GetSize() != 0)
+                return false;
+            else return true;
+        }
+        public class Date
+        {
+            public readonly DateTime time;
+
+            public Date()
+            {
+                time = DateTime.Now;
+            }
+        }
+    }
+}
---
+++ 8 лаба/8laba/OOP_Lab4/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyList<string> list = new MyList<string>();
+            Console.WriteLine("List one:");
+            Console.WriteLine("--------------------------");
+            list.Add("Ivan");
+            list.Add("Andrew");
+            list.Add("Natalia");
+            list.Add("Victor");
+            list.Show();
+            Console.WriteLine("--------------------------");
+
+            MyList<string> list1 = new MyList<string>();
+            Console.WriteLine("List two");
+            Console.WriteLine("--------------------------");
+            list1.Add("Alexander");
+            list1.Add("Uri");
+            list1.Add("Vladimir");
+            list1.Add("Sergey");
+            list1.Show();
+            Console.WriteLine("--------------------------");
+            bool cn = (list1 == list);
+            Console.WriteLine("Списки равны:" + cn);
+            list = list + list1;
+            Console.WriteLine("Combined list");
+            Console.WriteLine("--------------------------");
+            list.Show();
+            Console.WriteLine("--------------------------");
+
+            list--;
+            list = list + "Andrey";
+
+
+            MyList<int> list3 = new MyList<int>();
+            Console.WriteLine("Int list:");
+            Console.WriteLine("--------------------------");
+            list3.Add(1);
+            list3.Add(4);
+            list3.Add(3);
+            list3.Add(7);
+            list3.Show();
+            Console.WriteLine("--------------------------");
+
+            MyList<int> list5 = new MyList<int>();
+            if (list5)
+                Console.WriteLine("List 5 is empty");
+
+            string abc = "awdg fseol  ajes";
+            Console.WriteLine($"Count of words: {abc.CountOfWords()}");
+
+
+
+
+            MyList<Person> list4 = new MyList<Person>();
+            Person tom = new Person();
+            tom.Name = "Tom";
+            tom.Age = 18;
+            list4.Add(tom);
+            Console.ReadKey();
+        }
+    }
+}
